8703c2477000a9b25b87f18dbc914c0037ad6e53
seon <seonsy44@gmail.com>
2022-05-27 00:54:41 +0900

    feat : BEST탭 구현

---
+++ front/src/pages/Main/BestProductCard.js
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as fullHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as Heart } from "@fortawesome/free-regular-svg-icons";
+
+import { useNavigate } from "react-router-dom";
+
+const BestProductCard = ({ product, index }) => {
+  const navigate = useNavigate();
+  const [wish, setWish] = useState(product.wish);
+
+  return (
+    <Container wish={wish}>
+      <span>{index + 1}</span>
+      <img
+        src={product.images[0]}
+        alt="img"
+        onClick={() => {
+          navigate(`/products/${product.id}`);
+        }}
+      />
+      <Information
+        onClick={() => {
+          navigate(`/products/${product.id}`);
+        }}
+      >
+        <Title>
+          <span>[{product.store}]</span>
+          <span>{product.name}</span>
+        </Title>
+        <Price>
+          <span>{product.price}원</span>
+          <div className="sale-price">
+            <span>
+              {Math.round((product.price - product.salePrice) / 100)}%
+            </span>
+            <span>{product.salePrice}원</span>
+          </div>
+        </Price>
+      </Information>
+      <span
+        onClick={() => {
+          setWish((cur) => !cur);
+        }}
+      >
+        {wish && <FontAwesomeIcon icon={fullHeart} size="1x" />}
+        {!wish && <FontAwesomeIcon icon={Heart} size="1x" />}
+      </span>
+    </Container>
+  );
+};
+
+export default BestProductCard;
+
+const Container = styled.div`
+  position: relative;
+  border-bottom: #cdcdcd solid 1px;
+  width: 100%;
+  height: 120px;
+  display: flex;
+  align-items: center;
+
+  > span:first-child {
+    font-size: 20px;
+    font-weight: 500;
+    width: 40px;
+    text-align: center;
+  }
+
+  > span:last-child {
+    cursor: pointer;
+    position: absolute;
+    right: 0;
+    bottom: 14px;
+    color: ${({ wish }) => {
+      if (wish) return "#FF6A6A;";
+      else return "#9c9c9c;";
+    }};
+  }
+
+  img {
+    cursor: pointer;
+    width: 85px;
+    height: 85px;
+    border-radius: 10px;
+  }
+`;
+
+const Information = styled.div`
+  cursor: pointer;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  height: 100%;
+  flex-grow: 1;
+  padding: 18px 0 18px 14px;
+`;
+
+const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 15px;
+  > span {
+    margin-bottom: 3px;
+  }
+`;
+
+const Price = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  > span {
+    font-size: 12px;
+    color: #b1b1b1;
+    text-decoration-line: line-through;
+  }
+
+  .sale-price {
+    margin-top: 3px;
+    font-size: 15px;
+    font-weight: 600;
+
+    > span:first-child {
+      color: #ffb564;
+      margin-right: 3px;
+    }
+  }
+`;
--- front/src/pages/Main/BestTab.js
+++ front/src/pages/Main/BestTab.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
+
+import BestProductCard from "./BestProductCard";
 
 const BestTab = () => {
-  return <Container></Container>;
+  const [products, setProducts] = useState([]);
+
+  const getProductData = async () => {
+    const data = await axios("/data/productsList.json", { method: "GET" });
+    setProducts(data.data.productList);
+  };
+
+  useEffect(() => {
+    getProductData();
+  }, []);
+
+  return (
+    <Container>
+      <Title>
+        <span>잘나가는 판매상품 </span>
+        <span>BEST 10</span>
+      </Title>
+      {products.map((product, index) => (
+        <BestProductCard product={product} index={index} key={product.id} />
+      ))}
+    </Container>
+  );
 };
 
 export default BestTab;
 
 const Container = styled.div`
-  border: red solid 1px;
   position: relative;
-  margin-top: 20px;
-  width: 90%;
+  margin-top: 25px;
+  margin-bottom: 120px;
+  width: 84%;
   min-height: 90vh;
-  left: 5%;
+  left: 8%;
+`;
+
+const Title = styled.div`
+  margin-bottom: 10px;
+  font-size: 22px;
+  font-weight: 600;
+
+  span:last-child {
+    color: #ff911b;
+  }
 `;
