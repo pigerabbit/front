0fa21407fba4580b187dbc8fda6f673420c04f70
bsy1141 <bsy1141@naver.com>
2022-05-28 20:52:21 +0900

    feat: PurchaseList InfoWrapper(count, filter-dropbox) 구현

--- front/src/pages/My/MyPurchaseList/ParticipatePurchaseListTab.js
+++ front/src/pages/My/MyPurchaseList/ParticipatePurchaseListTab.js
@@ -1,5 +1,79 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { FakeParticipategroupList } from "../MyMockData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+
+const options = ["전체보기", "진행중", "결제완료"];
+
 const ParticipatePurchaseListTab = () => {
-  return <h1>내가 참여한 공구 탭</h1>;
+  const [option, setOption] = useState("전체보기");
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Container>
+      <InfoWrapper>
+        <p>
+          총 <strong>{FakeParticipategroupList.length}</strong>개
+        </p>
+        <SelectBox>
+          <span>{option}</span>
+          <OpenButton onClick={() => setIsOpen(!isOpen)}>
+            <FontAwesomeIcon icon={faCaretDown} />
+          </OpenButton>
+          {options.map((option) => (
+            <Option
+              key={option}
+              onClick={() => setOption(option)}
+              open={isOpen ? "block" : "none"}
+            >
+              {option}
+            </Option>
+          ))}
+        </SelectBox>
+      </InfoWrapper>
+    </Container>
+  );
 };
 
 export default ParticipatePurchaseListTab;
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 770px;
+  min-width: 360px;
+  height: 100%;
+  margin-top: 10px;
+  overflow-y: scroll;
+  -ms-overflow-style: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 2%;
+`;
+
+const SelectBox = styled.div`
+  position: relative;
+  display: inline-block;
+  border: 1px solid #c4c4c4;
+  border-radius: 5px;
+  padding: 5px 0 5px 5px;
+`;
+
+const Option = styled.div`
+  display: ${(props) => props.open};
+  postion: abosolute;
+  cursor: pointer;
+`;
+
+const OpenButton = styled.button`
+  border: none;
+  background: transparent;
+  cursor: pointer;
+`;
