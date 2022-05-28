4973c2153f1ea08cfea9d347db04caa099d2d732
eunna <dladmssk98@gmail.com>
2022-05-28 23:18:43 +0900

    feat : review 탭 구현 시작

--- front/src/pages/ProductDetail/ProductReviewTab.js
+++ front/src/pages/ProductDetail/ProductReviewTab.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
+import ProductReviewCard from "./ProductReviewCard";
+
 const ProductReview = ({ product }) => {
-  return <Container>{product.name}후기후기</Container>;
+  const [reviews, setReviews] = useState([]);
+  const n = reviews.length;
+
+  return (
+    <Container>
+      <WriteButton>후기 작성하기</WriteButton>
+      <Review>
+        <ReviewTop>
+          <div id="reviewCount">후기 {n}건</div>
+          <MyReviewButton>내 후기</MyReviewButton>
+        </ReviewTop>
+      </Review>
+    </Container>
+  );
 };
 
 export default ProductReview;
@@ -13,8 +28,59 @@ const Container = styled.div`
   min-width: 360px;
   max-width: 770px;
   background-color: #ffffff;
-  "::-webkit-scrollbar-track" {
-    background: none;
-  }
   margin-top: 7px;
+`;
+
+const WriteButton = styled.div`
+  border: 1px solid #636363;
+  border-radius: 10px;
+  width: 90%;
+  height: 50px;
+  margin: 25px auto;
+  font-size: 16px;
+  font-weight: bold;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f0f0f0;
+  }
+`;
+
+const Review = styled.div`
+  width: 90%;
+  margin: 20px auto;
+`;
+
+const ReviewTop = styled.div`
+  padding-bottom: 20px;
+  border-bottom: 2px solid #000000;
+  font-weight: bold;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  .reviewCount {
+    position: absolute;
+    left: 5%;
+  }
+`;
+
+const MyReviewButton = styled.div`
+  position: absolute;
+  right: 5%;
+  width: 80px;
+  height: 20px;
+  border: 1px solid #d0d0d0;
+  border-radius: 5px;
+  box-shadow: 1px 1px #d0d0d0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-size: 13px;
+  cursor: pointer;
 `;
