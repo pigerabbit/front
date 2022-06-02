55cde2d1a28c4b379c7f007ffaa22a8e249ff567
seon <seonsy44@gmail.com>
2022-06-02 21:05:45 +0900

    modify: Current User가 없는 경우에 api 요청

--- front/src/FetchCurrentUser.js
+++ front/src/FetchCurrentUser.js
@@ -1,19 +1,22 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { login } from "redux/userSlice";
 import * as Api from "api";
 
 const FetchCurrentUser = ({ children }) => {
   const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
   const navigate = useNavigate();
 
   const fetchCurrentUser = async () => {
     try {
-      const res = await Api.get("user/current");
-      const currentUser = res.data.payload;
+      if (!user) {
+        const res = await Api.get("user/current");
+        const currentUser = res.data.payload;
 
-      dispatch(login(currentUser));
+        dispatch(login(currentUser));
+      }
     } catch {
       navigate("/login");
     }
