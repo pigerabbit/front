643b431e65a4478153327386de4afe8f799a245d
eunna <dladmssk98@gmail.com>
2022-06-04 21:44:28 +0900

    feat : 그룹 참여 기능 세팅

---
+++ front/src/pages/ProductDetail/JoinGroupWindow.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const JoinGroupWindow = () => {
+  return <Container>그룹 참여</Container>;
+};
+
+export default JoinGroupWindow;
+
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 770px;
+  min-width: 360px;
+  min-height: 100vh;
+  background-color: #d0d0d0;
+  opacity: 0.5;
+`;
