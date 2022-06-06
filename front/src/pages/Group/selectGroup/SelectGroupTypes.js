fcae5b1f792c35abfc878bccac804464a680de84
bsy1141 <bsy1141@naver.com>
2022-06-06 21:20:09 +0900

    fix: 공구 선택 페이지 약관 선택 안했을 시 버튼 비활성화 처리 & 픽업 공구 삭제

--- front/src/pages/Group/selectGroup/SelectGroupTypes.js
+++ front/src/pages/Group/selectGroup/SelectGroupTypes.js
@@ -4,24 +4,21 @@ import { useNavigate } from "react-router-dom";
 const groupTypes = {
   "지역 공구": ["동네로 보내드립니다.(동네에서 픽업)", "local"],
   "택배 공구": ["택배로 보내드립니다.(주소지로 배송)", "normal"],
-  "픽업 공구": ["상품의 판매처에서 직접 픽업합니다.", "pickup"],
   "이용권 공구": ["이용권을 공동 구매하여 사용합니다.", "ticket"],
 };
 
 const productTypes = {
   normal: ["지역 공구", "택배 공구"],
-  pickup: ["픽업 공구"],
   ticket: ["이용권 공구"],
 };
 
-const SelectGroupTypes = ({ type, product }) => {
+const SelectGroupTypes = ({ type, product, isChecked }) => {
   const navigate = useNavigate();
   return (
     <Container>
       {Object.entries(groupTypes).map(([key, value]) => (
         <ButtonContainer
-          key={key}
-          isActive={productTypes[type].includes(key)}
+          isActive={productTypes[type].includes(key) && isChecked}
           onClick={() =>
             navigate(`/group/open/${product.id}?type=${value[1]}`, {
               state: { product: product },
@@ -41,7 +38,6 @@ export default SelectGroupTypes;
 const Container = styled.div`
   width: 100%;
   padding: 0 10%;
-  margin-top: 5%;
   @media (max-width: 500px) {
     margin-top: 0;
   }
@@ -49,7 +45,7 @@ const Container = styled.div`
 
 const ButtonContainer = styled.button`
   display: inline-block;
-  width: 38%;
+  width: 80%;
   margin-right: 5%;
   background: #d9d9d9;
   color: #969696;
