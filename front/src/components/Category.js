922b99c32bc19e68b53b8aa07fac63dc225425e0
seon <seonsy44@gmail.com>
2022-06-07 13:46:46 +0900

    feat: 이용권 카테고리메뉴 구현

--- front/src/components/Category.js
+++ front/src/components/Category.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -12,11 +12,21 @@ import {
   faFaceSmile,
   faUtensils,
   faCircleExclamation,
+  faBowlFood,
+  faMugSaucer,
+  faDumbbell,
+  faGem,
+  faHotel,
+  faDog,
+  faPencil,
+  faHand,
+  faGamepad,
+  faTicket,
 } from "@fortawesome/free-solid-svg-icons";
 
 import { useNavigate } from "react-router-dom";
 
-const category = [
+const parcelCategory = [
   { eng: "fruitVegetable", kor: "과일·채소", icon: faCarrot },
   { eng: "cereal", kor: "견과·쌀", icon: faWheatAwn },
   { eng: "seafood", kor: "수산·해산·건어물", icon: faFish },
@@ -29,7 +39,29 @@ const category = [
   { eng: "other", kor: "기타", icon: faCircleExclamation },
 ];
 
+const subscribeCategory = [
+  { eng: "food", kor: "음식", icon: faBowlFood },
+  { eng: "cafe", kor: "카페·디저트", icon: faMugSaucer },
+  { eng: "health", kor: "운동·헬스", icon: faDumbbell },
+  { eng: "beauty", kor: "미용·뷰티·네일", icon: faGem },
+  { eng: "stay", kor: "숙박·펜션·모텔", icon: faHotel },
+  { eng: "pet", kor: "애견·반려동물", icon: faDog },
+  { eng: "study", kor: "스터디·학원·교육", icon: faPencil },
+  { eng: "class", kor: "공방·클래스", icon: faHand },
+  { eng: "game", kor: "오락·여가·레저", icon: faGamepad },
+  { eng: "culture", kor: "문화·예술", icon: faTicket },
+  { eng: "other", kor: "기타", icon: faCircleExclamation },
+];
+
+const options = [
+  { eng: "parcel", kor: "택배", categoryType: parcelCategory },
+  { eng: "subscribe", kor: "이용권", categoryType: subscribeCategory },
+];
+
 const Category = ({ setIsOpenSideBar, setProducts, setPage }) => {
+  const [option, setOption] = useState("parcel");
+  const [category, setCategory] = useState(parcelCategory);
+
   const navigate = useNavigate();
 
   const handleClick = (eng) => {
@@ -45,12 +77,29 @@ const Category = ({ setIsOpenSideBar, setProducts, setPage }) => {
 
   return (
     <Container>
-      {category.map(({ eng, kor, icon }, idx) => (
-        <span key={idx} onClick={handleClick(eng)}>
-          <FontAwesomeIcon icon={icon} />
-          {kor}
-        </span>
-      ))}
+      <TabContainer>
+        {options.map(({ eng, kor, categoryType }) => (
+          <Tab
+            key={eng}
+            selected={eng === option}
+            onClick={() => {
+              setOption(eng);
+              setCategory(categoryType);
+            }}
+          >
+            {kor}
+          </Tab>
+        ))}
+      </TabContainer>
+
+      <CategoryContainer>
+        {category.map(({ eng, kor, icon }, idx) => (
+          <span key={idx} onClick={handleClick(eng)}>
+            <FontAwesomeIcon icon={icon} />
+            {kor}
+          </span>
+        ))}
+      </CategoryContainer>
     </Container>
   );
 };
@@ -62,18 +111,44 @@ const Container = styled.div`
   width: 100%;
   height: 90vh;
   box-sizing: border-box;
-  padding: 10vh 0;
+
+  font-size: 20px;
+  @media (min-width: 500px) {
+    font-size: 22px;
+  }
+  @media (min-width: 770px) {
+    font-size: 24px;
+  }
+`;
+
+const TabContainer = styled.div`
+  top: 0;
+  width: 100%;
+  height: 6vh;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+const Tab = styled.div`
+  cursor: pointer;
+  color: ${({ selected }) => (selected ? "#F79831;" : "#ABABAB;")};
+  border-bottom: ${({ selected }) =>
+    selected ? "3px solid #F79831;" : "3px solid white;"};
+
+  & + div {
+    margin-left: 2vw;
+  }
+`;
+
+const CategoryContainer = styled.div`
+  width: 100%;
+  height: 72vh;
+  margin-top: 5vh;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  font-size: 22px;
-  @media (min-width: 500px) {
-    font-size: 24px;
-  }
-  @media (min-width: 770px) {
-    font-size: 28px;
-  }
 
   > span {
     cursor: pointer;
@@ -83,7 +158,7 @@ const Container = styled.div`
     }
 
     > svg {
-      margin-right: 10px;
+      margin-right: 15px;
     }
   }
 `;
