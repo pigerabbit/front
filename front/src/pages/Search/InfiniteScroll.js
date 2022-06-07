95406984ca43593990edc0c4ae99ef10e96480d6
seon <seonsy44@gmail.com>
2022-06-07 22:26:55 +0900

    modify: 인피니트 스크롤, 모바일 화면에서도 되도록 수정

--- front/src/pages/Search/InfiniteScroll.js
+++ front/src/pages/Search/InfiniteScroll.js
@@ -22,6 +22,8 @@ const InfiniteScroll = ({ setPage }) => {
 export default InfiniteScroll;
 
 const Ref = styled.div`
+  border: 3px solid white;
+  z-index: 1;
   position: absolute;
   bottom: 0;
 `;
