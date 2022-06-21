f92c5ea385690c0c4d13b1440dd65353e8c95174
bsy1141 <bsy1141@naver.com>
2022-06-22 00:55:30 +0900

    fix: 최근 본 상품 프론트에서 필터링 삭제

--- src/pages/Search/SearchCurrent.js
+++ src/pages/Search/SearchCurrent.js
@@ -26,11 +26,11 @@ const SearchCurrent = () => {
       ]);
 
       setCurrentKeyword(currentKeyword.data.reverse());
-      const filteredProductList = productList.data.filter(
-        (arr, index, callback) =>
-          index === callback.findIndex((t) => t.id === arr.id)
-      );
-      setProductList(filteredProductList);
+      // const filteredProductList = productList.data.filter(
+      //   (arr, index, callback) =>
+      //     index === callback.findIndex((t) => t.id === arr.id)
+      // );
+      setProductList(productList.data);
 
       setLoading(false);
     } catch (err) {
