b5df651238e84ac65dc7c9eb4af28d2d418b293f
seon <seonsy44@gmail.com>
2022-06-22 01:44:16 +0900

    modify: sideBar position 문제 해결

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled, { keyframes } from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
@@ -7,6 +7,7 @@ import usePreventScroll from "hooks/usePreventScroll";
 
 const SideBar = ({ children, title, setIsOpenSideBar }) => {
   const [show, setShow] = useState(true);
+  const [top, setTop] = useState(window.scrollY);
 
   const handleCancelClick = () => {
     setShow(false);
@@ -18,7 +19,7 @@ const SideBar = ({ children, title, setIsOpenSideBar }) => {
   usePreventScroll();
 
   return (
-    <Container show={show}>
+    <Container show={show} top={top}>
       <TitleBar>
         <div></div>
         <span>{title}</span>
@@ -52,13 +53,14 @@ const sidebarUnshow = keyframes`
 `;
 
 const Container = styled.div`
-  position: fixed;
-  top: 0;
+  position: absolute;
+  top: ${({ top }) => top + "px;"}
+  left: 0;
   width: 100%;
+  max-width: 770px;
   height: 100vh;
   background-color: #f6f6f6;
   z-index: 12;
-  animation: ${sidebarShow} 0.5s;
   animation: ${({ show }) => (show ? sidebarShow : sidebarUnshow)} 0.5s;
 `;
 
