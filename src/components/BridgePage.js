74e5aa5209f1bc7929781312a3102e56d611522d
eunna <dladmssk98@gmail.com>
2022-06-22 15:46:32 +0900

    feat : bridge page에서 payment 정보 & quantity 넘겨줌

--- src/components/BridgePage.js
+++ src/components/BridgePage.js
@@ -9,18 +9,20 @@ const BridgePage = () => {
   const searchParams = new URLSearchParams(location.search);
   const groupObjId = searchParams.get("group");
   const userId = searchParams.get("user");
+  const quantity = searchParams.get("quantity");
 
   const getPaymentDetail = async () => {
     try {
       const res = await Api.get(`payments/${groupObjId}/${userId}`);
       if (res.data.success) {
         try {
-          const group = res.data.payload.group;
-          console.log(res.data.payload);
+          const payment = res.data.payload;
           const paymentId = res.data.payload.paymentId;
           const resUse = await Api.put(`payments/${paymentId}`, { used: true });
           if (resUse.data.success) {
-            navigate("/check/result", { state: { success: true } });
+            navigate("/check/result", {
+              state: { success: true, payment, quantity },
+            });
           }
         } catch (e) {
           navigate("/check/result", { state: { success: false } });
