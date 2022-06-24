96714949e538218ede353f92e4d9ee0a7c70ee29
bsy1141 <bsy1141@naver.com>
2022-06-24 18:59:17 +0900

    리뷰에 따라 테스트 코드 수정

--- src/utils/game_369.test.js
+++ src/utils/game_369.test.js
@@ -1,45 +1,95 @@
 import game_369 from "./game_369";
 
-describe("369game function", () => {
-  it("return 박수 if input includes 3,6,9", () => {
-    const num = 13;
-    const expected = "박수";
+describe("game_369 function", () => {
+  describe("with a positive integer", () => {
+    it("when it is Number and includes 3, return 박수", () => {
+      const num = 134;
+      const expected = "박수";
 
-    const result = game_369(num);
+      const result = game_369(num);
 
-    expect(result).toBe(expected);
+      expect(result).toBe(expected);
+    });
+
+    it("when it is Number and includes 6, return 박수", () => {
+      const num = 66;
+      const expected = "박수";
+
+      const result = game_369(num);
+
+      expect(result).toBe(expected);
+    });
+
+    it("when it is Number and includes 9, return 박수", () => {
+      const num = 10009;
+      const expected = "박수";
+
+      const result = game_369(num);
+
+      expect(result).toBe(expected);
+    });
+
+    it("when it is String that can be converted to a Number and includes 3 or 6 or 9, return 박수", () => {
+      const num = "123";
+      const expected = "박수";
+
+      const result = game_369(num);
+
+      expect(result).toBe(expected);
+    });
+
+    it("when it is Number and not includes 3 or 6 or 9, return input value", () => {
+      const num = 112;
+      const expected = 112;
+
+      const result = game_369(num);
+
+      expect(result).toBe(expected);
+    });
+
+    it("when it is String that can be converted to a Number and not includes 3 or 6 or 9, return input value", () => {
+      const num = "1211";
+      const expected = "1211";
+
+      const result = game_369(num);
+
+      expect(result).toBe(expected);
+    });
   });
+  describe("with a non positive integer", () => {
+    it("when it is Infinity, return 게임을 진행할 수 없습니다", () => {
+      const num = Infinity;
+      const expected = "게임을 진행할 수 없습니다";
 
-  it("return input if not include 3,6,9", () => {
-    const num = 10;
-    const expected = 10;
+      const result = game_369(num);
 
-    const result = game_369(num);
+      expect(result).toBe(expected);
+    });
 
-    expect(result).toBe(expected);
-  });
+    it("when it is -Infinity, return 게임을 진행할 수 없습니다", () => {
+      const num = -Infinity;
+      const expected = "게임을 진행할 수 없습니다";
 
-  it("return else if input is string not to be replaced by Number", () => {
-    const num = "abc";
-    const expected = "숫자가 아니거나 무한대입니다.";
+      const result = game_369(num);
 
-    const result = game_369(num);
+      expect(result).toBe(expected);
+    });
+    it("when it is not positive number, return 게임을 진행할 수 없습니다", () => {
+      const num = -123;
+      const expected = "게임을 진행할 수 없습니다";
 
-    expect(result).toBe(expected);
-  });
+      const result = game_369(num);
 
-  it("return else if input is string that can be replaced by Number", () => {
-    const num = "123";
-    const expected = "박수";
+      expect(result).toBe(expected);
+    });
 
-    const result = game_369(num);
-    expect(result).toBe(expected);
-  });
+    it("when it is not integer, return 게임을 진행할 수 없습니다", () => {
+      const num = 1.9;
+      const expected = "게임을 진행할 수 없습니다";
 
-  // 1) 테스트 문장을 최대한 나누어서 두개의 문장으로 작성하는것 2) 하나의 문장에 두 개의 테스트 예시를 넣는 것
-  // 두 방법 모두 테스트 기능 상에는 문제가 없는 것 같아서 어느 방법이 더 나은 테스트인지 궁금합니다
-  it("return else if input is Infinity or -Infinity", () => {
-    expect(game_369(Infinity)).toBe("숫자가 아니거나 무한대입니다.");
-    expect(game_369(-Infinity)).toBe("숫자가 아니거나 무한대입니다.");
+      const result = game_369(num);
+
+      expect(result).toBe(expected);
+    });
   });
 });
