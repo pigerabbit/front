f2fcf17105dabafb8d8ea637a4d9b60aa25d687a
seon <seonsy44@gmail.com>
2022-07-01 16:15:16 +0900

    modify: 카카오 로그인 수정

--- src/pages/User/KakaoRedirectHandler.js
+++ src/pages/User/KakaoRedirectHandler.js
@@ -1,12 +1,16 @@
+import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const KakaoRedirectHandler = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const params = new URLSearchParams(location.search);
-  const token = params.get("token");
-  sessionStorage.setItem("userToken", token);
-  navigate("/");
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const token = params.get("token");
+    sessionStorage.setItem("userToken", token);
+    navigate("/");
+  }, []);
 
   return <div>redirect page</div>;
 };
